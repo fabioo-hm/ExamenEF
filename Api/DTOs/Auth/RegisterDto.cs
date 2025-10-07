76f7572a2c7e48905bc34905f30bed63d42291a5
fabioo-hm <194815533+fabioo-hm@users.noreply.github.com>
2025-10-07 18:24:37 -0500

    feat: :sparkles: Add Auth DTOs

---
+++ Api/DTOs/Auth/RegisterDto.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.DTOs.Auth;
+
+public class RegisterDto
+{
+    [Required]
+    public string? Email { get; set; }
+    [Required]
+    public string? Username { get; set; }
+    [Required]
+    public string? Password { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+}
