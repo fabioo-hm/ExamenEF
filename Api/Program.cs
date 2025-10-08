92cca0226ef0e730ff44ed86f95983344c86732d
davidsc21 <194646074+davidsc21@users.noreply.github.com>
2025-10-08 12:08:58 -0500

    feat: :sparkles: Add Program

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,9 @@
+
+using Api.Extensions;
+using Api.Helpers.Errors;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,18 +11,37 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.ConfigureCors();
+builder.Services.AddCustomRateLimiter();
+builder.Services.AddJwt(builder.Configuration);
+builder.Services.AddApplicationServices();
+builder.Services.AddDbContext<AutoTallerDbContext>(options =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("Postgres")!;
+    options.UseNpgsql(connectionString);
+    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+});
+
+// builder.Services.AddDbContext<AppDbContext>(opt =>
+//     opt.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")));
 
 var app = builder.Build();
-
+app.UseMiddleware<ExceptionMiddleware>();
+await app.SeedRolesAsync();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+app.UseCors("CorsPolicy");
+app.UseCors("CorsPolicyUrl");
+app.UseCors("Dinamica");
 
 app.UseHttpsRedirection();
+app.UseRateLimiter();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllers();
 
